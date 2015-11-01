a4c61c8c88d2b3b33f3467af537aa32638f57337
Dragos MOCA <dmoca@e1p63.cluj.42.fr>
2015-11-01 15:24:09 +0100

    Functie

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,37 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmoca <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/10/23 21:07:37 by dmoca             #+#    #+#             */
-/*   Updated: 2015/10/23 21:28:10 by dmoca            ###   ########.fr       */
+/*   Created: 2015/11/01 15:06:55 by dmoca             #+#    #+#             */
+/*   Updated: 2015/11/01 15:20:11 by dmoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	j;
+	size_t i;
+	size_t j;
+	size_t len;
 
+	len = ft_strlen(dst);
 	i = 0;
-	j = 0;
-	while (dst[i])
+	while(dst[i])
 		i++;
-	if (size < ft_strlen(dst))
-		return (ft_strlen(dst) + size);
-	else if (size == ft_strlen(dst))
-	{
-		return (ft_strlen(dst) + ft_strlen(src));
-	}
+	j = 0;
+	if (size <= len)
+		return (ft_strlen(src) + size);
 	else
 	{
-		while (src[j] && j < size - ft_strlen(dst) - 1)
+		while(src[j] && j < size - len - 1)
 		{
 			dst[i] = src[j];
 			j++;
 			i++;
 		}
 		dst[i] = '\0';
-		return (ft_strlen(dst) + ft_strlen(src));
+		return (len + ft_strlen(src));
 	}
 }
