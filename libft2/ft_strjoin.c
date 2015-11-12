ab797b0cb801e46f552509f284857269103fd492
Dragos MOCA <dmoca@e1p56.cluj.42.fr>
2015-11-12 20:02:22 +0100

    Functii

---
+++ libft2/ft_strjoin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmoca <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/31 19:41:34 by dmoca             #+#    #+#             */
+/*   Updated: 2015/11/01 10:05:50 by dmoca            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	if (!(str = (char*)malloc(sizeof(char) * (i + j + 1))))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
