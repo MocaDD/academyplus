359674198416399c6bbe78dbc447a962e33ca8aa
Dragos MOCA <dmoca@e1p61.cluj.42.fr>
2015-11-29 17:24:23 +0100

    Facut

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,120 +1,50 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jfazakas <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/09 17:50:08 by jfazakas          #+#    #+#             */
-/*   Updated: 2015/11/13 12:14:23 by jfazakas         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "get_next_line.h"
 
-static int	copy_to_line(char **line, char *source, int new_size)
+static int		read_to_stock(int const fd, char **stock)
 {
-	char	*new_memory;
-	int		old_size;
-	int		index;
-
-	old_size = ft_strlen(*line);
-	if (!(new_memory = ft_strnew(old_size + new_size)))
-		return (-1);
-	index = 0;
-	while (index < old_size)
-	{
-		new_memory[index] = (*line)[index];
-		index++;
-	}
-	index = 0;
-	while (index < new_size)
-	{
-		new_memory[old_size + index] = source[index];
-		index++;
-	}
-	ft_strdel(line);
-	*line = new_memory;
-	return (1);
-}
-
-static int	select_and_copy(char **line,
-		char *source, int *eol_boolean)
-{
-	int		new_size;
-
-	new_size = 0;
-	while (source[new_size] != '\0' && source[new_size] != '\n')
-		new_size++;
-	if (copy_to_line(line, source, new_size) == -1)
-		return (-1);
-	if (source[new_size] == '\n')
-		*eol_boolean = 1;
-	return (new_size);
-}
-
-static int	save_and_exit(char **save, char *source, int used_size)
-{
+	char	*buff;
+	int		ret;
 	char	*temp;
 
-	temp = ft_strdup(source + used_size + 1);
-	if (!temp)
+	if (!(buff = (char *)malloc(sizeof(*buff) * (BUFF_SIZE + 1))))
 		return (-1);
-	ft_strdel(save);
-	*save = temp;
+	ret = read(fd, buff, BUFF_SIZE);
+	if (ret > 0)
+	{
+		buff[ret] = '\0';
+		temp = ft_strjoin(*stock, buff);
+		free(*stock);
+		*stock = temp;
+	}
+	free(buff);
+	return (ret);
+}
+
+int				get_next_line(int const fd, char **line)
+{
+	static char	*stock = NULL;
+	char		*bn;
+	int			ret;
+
+	if ((!stock && (stock = (char *)malloc(sizeof(*stock))) == NULL) || !line
+			|| fd < 0 || BUFF_SIZE < 0)
+		return (-1);
+	bn = ft_strchr(stock, '\n');
+	while (bn == NULL)
+	{
+		ret = read_to_stock(fd, &stock);
+		if (ret == 0)
+		{
+			if (ft_strlen(stock) == 0)
+				return (0);
+			stock = ft_strjoin(stock, "\n");
+		}
+		if (ret < 0)
+			return (-1);
+		else
+			bn = ft_strchr(stock, '\n');
+	}
+	*line = ft_strsub(stock, 0, ft_strlen(stock) - ft_strlen(bn));
+	stock = ft_strdup(bn + 1);
 	return (1);
-}
-
-int			get_that_line(int const fd, char **line, char **save)
-{
-	char	*buffer;
-	int		new_size;
-	int		eol_boolean;
-	long	read_return;
-
-	eol_boolean = 0;
-	*line = ft_strdup("");
-	if ((new_size = select_and_copy(line, *save, &eol_boolean)) == -1)
-		return (-1);
-	if (eol_boolean == 1)
-		return (save_and_exit(save, *save, new_size));
-	buffer = ft_strnew(BUFF_SIZE);
-	while (!eol_boolean)
-	{
-		ft_strclr(buffer);
-		read_return = read(fd, buffer, BUFF_SIZE);
-		if (read_return == -1 || read_return == 0)
-			return (read_return);
-		if (read_return < BUFF_SIZE)
-			if (*(ft_strchr(buffer, 0) - 1) != '\n')
-				buffer[ft_strlen(buffer)] = '\n';
-		if ((new_size = select_and_copy(line, buffer, &eol_boolean)) == -1)
-			return (-1);
-	}
-	return (save_and_exit(save, buffer, new_size));
-}
-
-int			get_next_line(int const fd, char **line)
-{
-	static t_list	*list = NULL;
-	t_list			*node;
-
-	if (fd < 0 || !line)
-		return (-1);
-	node = list;
-	while (node && node->FILE_DESCRIPTOR != (unsigned int)fd)
-		node = node->next;
-	if (!node)
-	{
-		node = (t_list*)malloc(sizeof(t_list));
-		if (!node)
-			return (-1);
-		node->FILE_DESCRIPTOR = fd;
-		node->SAVE = ft_strdup("");
-		node->next = NULL;
-		ft_lstadd(&list, node);
-	}
-	if (!list)
-		list = ft_lstnew(ft_strdup(""), fd);
-	return (get_that_line(fd, line, (char**)&(node->SAVE)));
 }
